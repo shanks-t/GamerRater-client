c7b2ac77afc12dc77f1a507553ea89d1a9959066
trey shanks <shanksleslietrey@gmail.com>
2021-11-10 10:04:29 -0600

    first commit

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Link, useHistory } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const history = useHistory()
+    return (
+        <ul className="navbar">
+            <li className="navbar__item">
+                <Link className="nav-link" to="/games">Games</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="nav-link" to="/events">Events</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="nav-link" to="/profile">Profile</Link>
+            </li>
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("lu_token")
+                                history.push({ pathname: "/" })
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
