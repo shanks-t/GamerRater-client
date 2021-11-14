1aafb7c10c3de6408f67f5126ef7ec85ee22a877
trey shanks <shanksleslietrey@gmail.com>
2021-11-14 15:22:05 -0600

    ratings submit working

--- src/components/game/GameDetails.js
+++ src/components/game/GameDetails.js
@@ -4,7 +4,9 @@ import { getGame, addRating } from "./GameManager.js"
 
 export const GameDetails = (props) => {
     const [game, setGameState] = useState({})
-    const [ rating, setRating ] = useState(0)
+    const [ ratingObj, setRatingObj ] = useState({
+        "rating": 0
+    })
     const  { gameId } = useParams()
 
     useEffect(() => {
@@ -14,17 +16,19 @@ export const GameDetails = (props) => {
     useEffect(() => {
         console.log('game', game)
         console.log('id', gameId)
-    }, [game]);
+        console.log('rating', ratingObj)
+    }, [game, ratingObj]);
 
     const getRatingsState = (event) => {
-        const newRating = event.target.value
-        setRating(newRating)
+        const copyRating = { ...ratingObj }
+        copyRating['rating'] = event.target.value
+        setRatingObj(copyRating)
     }
-
+    
     const submitRating = (event) => {
         event.preventDefault()
         
-        addRating(gameId, rating)
+        addRating(gameId, ratingObj)
     }
     return (
         <article className="games">
@@ -47,8 +51,11 @@ export const GameDetails = (props) => {
                         <Link to={`${game.id}/review`}>Review Game</Link> <br></br>
                         <Link to={`games/edit/${game.id}`}>Edit Game</Link>
                         <div>
-                            <input type='range' min='1' max='10' onChange={(event) => getRatingsState(event)}></input>
-                            <p>Rating: {rating}</p>
+                            <input type='range' min='1' max='10' defaultValue='10' onChange={(event) => getRatingsState(event)}></input>
+                            {ratingObj.rating ?
+                                <p>Rating: {ratingObj.rating}</p>
+                                : <p>Rating: Rate this game</p>
+                            }
                             <button onClick={(event) => {
                                 submitRating(event)
                             }}>Submit Rating for {game.title}</button>
